1936e6817fc9220e58fb3d06ce5af1d849e75892
xiongkun <1490707861@qq.com>
2018-03-27 22:11:58 +0800

    add a new Problem

---
+++ JZoffer/Problem_missNum.java
@@ -0,0 +1,19 @@
+public class Problem_missNum {
+
+	//数组中未出现的最小正整数，正整数从1开始
+	public static int missNum(int[] arr) {
+		int l = 0; //表示遍历到目前为止已经包含的正整数范围[1, l]
+		int r = arr.length; //表示遍历到目前为止，在后序出现最优状况的情况下，arr可能包含的正整数范围是[1,r]
+		while (l < r) {
+			if (arr[l] == l + 1) {
+				l++;
+			} else if (arr[l] <= l || arr[l] > r || arr[arr[l] - 1] == arr[l]) {
+				arr[l] = arr[--r];
+			} else {
+				swap(arr, l, arr[l] - 1);
+			}
+		}
+		return l + 1;
+	}
+
+}
